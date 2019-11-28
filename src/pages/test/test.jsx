0dd56c2f9c1f43ca4c4773df01221590bd982097
星空下的草地 <1585362826@qq.com>
2019-11-28 21:10:43 +0800

    条件渲染4种方式

--- src/pages/test/test.jsx
+++ src/pages/test/test.jsx
@@ -16,11 +16,23 @@ export default class Test extends Component {
     // Taro.redirectTo({url:'/pages/index/index'})
   }
 
+  getDom(){
+    return(<Button>按钮Dom</Button>)
+  }
   render () {
+    let dom=null;
+    dom = !true||<Button onClick={this.clickHandle}>跳转3</Button>
     return (
       <View className='index'>
-          <Image className="img" src={Img} />
-          <Button onClick={this.clickHandle}>跳转</Button>
+          <Image className='img' src={Img} />
+          {
+            true?<Button onClick={this.clickHandle}>跳转1</Button>:null
+          }
+          {
+            false||<Button onClick={this.clickHandle}>跳转2</Button>
+          }
+          {dom}
+          {this.getDom()}
       </View>
     )
   }
